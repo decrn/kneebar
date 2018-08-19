0c2099fe9c17fccf0d532a2e27ff045563d07db2
Denis Carnier <denis.carnier.w1416@student.hogent.be>
2018-08-19 16:57:14 +0200

    Add Account page

--- src/app/components/account/account.component.ts
+++ src/app/components/account/account.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
+import { DataService } from '../../providers/data.service';
+import { Component, Inject, OnInit } from '@angular/core';
+import { Comment } from '../../models/comment';
 
 @Component({
   selector: 'app-account',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountComponent implements OnInit {
 
-  constructor() { }
+  comments: Comment[];
+  user: User;
+
+  constructor(
+    @Inject('DataService') public ds: DataService
+  ) { }
 
   ngOnInit() {
+    this.ds.getCommentsByUser().subscribe(comments => this.comments = comments);
+    this.ds.getLoggedInUser().subscribe(u => this.user = u);
   }
 
 }
--- src/app/components/account/account.module.ts
+++ src/app/components/account/account.module.ts
@@ -1,10 +1,19 @@
+import { CommentViewModule } from '../comment-view/comment-view.module';
+import { RouterModule } from '@angular/router';
 import { AppCommonModule } from '../../app-common.module';
 import { NgModule } from '@angular/core';
 import { AccountComponent } from './account.component';
 
+const routes = [
+  { path: '', component: AccountComponent }
+];
+
+
 @NgModule({
   imports: [
-    AppCommonModule
+    RouterModule.forChild(routes),
+    AppCommonModule,
+    CommentViewModule,
   ],
   declarations: [AccountComponent]
 })
